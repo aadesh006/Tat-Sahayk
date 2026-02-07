0476145e5642664eee286745ca5aa7aa4eba3a99
aadesh006 <aadeshchaudhari14@gmail.com>
2026-02-07 19:00:14 +0530

    Deployment: Cloudinary Setup - Media Uploads on Cloudinary

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,12 +1,18 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     PROJECT_NAME: str
     DATABASE_URL: str
-    SECRET_KEY: str = "default_secret"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    SECRET_KEY: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    
+    CLOUDINARY_CLOUD_NAME: str = ""
+    CLOUDINARY_API_KEY: str = ""
+    CLOUDINARY_API_SECRET: str = ""
 
-    class Config:
-        env_file = ".env"
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        extra="ignore" #Ignores Docker variables to prevent crashes
+    )
 
 settings = Settings()
