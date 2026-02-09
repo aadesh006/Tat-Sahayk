5e49493bacede7fb48bfde8b989bd80719e6d200
Priyal Khandal <priyaalkhandal@gmail.com>
2026-02-09 18:04:38 +0530

    Dashboard and map

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -19,7 +19,7 @@ const SideBar = ({ isOpen, onClose }) => {
     <div>
       {isOpen && (
         <div 
-          className="fixed inset-0 bg-black/50 z-40 lg:hidden backdrop-blur-sm"
+          className="fixed inset-0 bg-black/50 lg:hidden backdrop-blur-sm z-[1000]"
           onClick={onClose}
         />
       )}
@@ -27,14 +27,14 @@ const SideBar = ({ isOpen, onClose }) => {
       <aside
         className={`
           flex flex-col h-screen bg-white shrink-0 w-64
-          fixed top-0 left-0 z-50 transition-transform duration-300 ease-in-out
+          fixed top-0 left-0 z-[1000] transition-transform duration-300 ease-in-out
           lg:translate-x-0 lg:relative lg:top-0
-          ${isOpen ? "translate-x-0" : "-translate-x-full"}
+          ${isOpen ? "translate-x-0 z-[1000]" : "-translate-x-full"}
         `}
       >
         <div className="flex items-center justify-between h-16 px-6 border-b border-blue-200 shrink-0">
            <span className="text-xl font-semibold font-sans bg-clip-text text-blue-600 tracking-wider truncate px-2 ">
-             Tat-Sahay
+             Tat-Sahayak
            </span>
            
            <button onClick={onClose} className="lg:hidden text-blue-500">
--- frontend/src/pages/MapPage.jsx
+++ frontend/src/pages/MapPage.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react";
+import {
+  MapContainer,
+  TileLayer,
+  CircleMarker,
+  Circle,
+  Popup,
+  Tooltip,
+} from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import { MapData } from "../services/storage.js";
 
 const MapPage = () => {
   return (
-    <div>MapPage</div>
-  )
-}
+    <div className="w-full">
+      <div className="flex justify-center w-full ">
+        <MapContainer
+          center={[15.8281, 78.0373]}
+          zoom={5}
+          minZoom={5}
+          className="lg:w-[80%] w-full z-[0]"
+          style={{ height: "80vh"  }}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+            url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+            style={{ brightnes: 1.8 }}
+          />
+          {MapData.map((prop) => (
+            <Circle
+              key={prop.id} 
+              center={prop.position}
+              radius={prop.radius}
+              pathOptions={{
+                color: prop.color,
+                fillColor: prop.color,
+                fillOpacity: 0.5,
+              }}
+            >
+              <Tooltip direction="top" offset={[0, -20]} opacity={1}>
+                {prop.name}
+              </Tooltip>
+            </Circle>
+          ))}
+        </MapContainer>
+      </div>
+    </div>
+  );
+};
 
-export default MapPage
+export default MapPage;
